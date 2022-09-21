4ad74b67a642db495a705cbb6a1149aab37f18b2
William Fernandes Pereira <williamfernandesp@outlook.com>
2022-09-21 17:36:50 -0300

    tudo convertido para php e header e footer ok

---
+++ Alimentação Saudável/alimsaudavel.php
@@ -0,0 +1,69 @@
+<?php include('../header/cabecalho.php');?>
+
+    <!--Conteúdo-->
+
+    <div class="card p-3 m-4 mx-auto" style="width: 50%">
+      <div class="text-center d-flex justify-content-center">
+        <img src="img/chef.png" style="height: 2rem" />
+        <p style="color: #236828">Alimentação Saudável</p>
+      </div>
+      <div>
+        <div>
+          <div class="card mb-3" style="max-width: 800px;">
+            <div class="row g-0">
+              <div class="col-md-4">
+                <img src="img/Grao-de-Bico-com-Bacalhau.png" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                  <h5 class="card-title">Salada de Grão de Bico com Bacalhau</h5>
+                  <p class="card-text">Uma deliciosa salada de grão de bico com Bacalhau, trazendo uma refeição muito saborosa e saudável. </p>
+                  <div>
+                    <button type="button" class="btn btn-outline-secondary">Ver mais</button>
+                  </div>
+                  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3" style="max-width: 800px;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="img/Berinjela-Recheada-com-Frango-FIT.png" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Berinjela Recheada com Frango FIT</h5>
+                <p class="card-text">Uma receita saborosa e ligth, berinjela de forno recheada com frango, ótimo prato pra quem segue uma dieta leve queira comer de tudo.</p>
+                <div>
+                  <button type="button" class="btn btn-outline-secondary">Ver mais</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="card mb-3" style="max-width: 800px;">
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="img/Omelete-de-Forno-Fit.png" class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">Omelete de Forno</h5>
+                <latina class="card-text">Aprenda a preparar Omelete de forno com legumes com esta excelente e fácil receita.</p>
+                <div>
+                  <button type="button" class="btn btn-outline-secondary">Ver mais</button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <button type="button" class="col-lg-2 mx-auto btn btn-outline-secondary">Ver mais</button>
+    </div>
+    
+
+    <!--Conteúdo-->
+
+    <?php include('../footer/rodape.php');?>
---
+++ Contato/contato.php
@@ -0,0 +1,28 @@
+<?php include('../header/cabecalho.php');?>
+
+    <!--Conteúdo-->
+
+    <div class="card p-3 m-4 mx-auto" style="width: 75%">
+      <div class="text-center d-flex justify-content-center">
+        <img src="img/chef.png" style="height: 2rem" />
+        <p style="color: #236828">Contato</p>
+      </div>
+      <div class="card-body">
+        <p class="lh-base">
+          Suas opiniões, dúvidas, críticas, elogios e sugestões são muito
+          importantes para nossa equipe.<br />
+          Todas as mensagens recebidas são lidas e nos ajudam a aprimorar os
+          serviços oferecidos, porém pedimos sua compreensão para o fato de que
+          devido ao grande número de e-mails recebidos diariamente, nem sempre é
+          possível oferecer um suporte individual para todos os usuários do
+          site.<br />
+          Para entrar em contato com a redação, envie um e-mail para
+          lorem@ipsum.com.br.<br />
+          Para contatos comerciais, envie um e-mail para lorem@ipsum.com.br
+        </p>
+      </div>
+    </div>
+
+    <!--Conteúdo-->
+
+    <?php include('../footer/rodape.php');?>
