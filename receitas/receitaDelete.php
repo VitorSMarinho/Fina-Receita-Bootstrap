ccbfb1f59022b797640a498cb1fdec51a815ae66
William Fernandes Pereira <williamfernandesp@outlook.com>
2022-11-09 17:29:59 -0300

    excluir receita pronto

--- receitas/receitaDelete.php
+++ receitas/receitaDelete.php
@@ -1,17 +1,17 @@
 <?php
 if (isset($_GET["id"]) && !empty($_GET["id"])) {
     include "../conexao/conexao.php";
-    $query = "delete from usuarios where id = " . $_GET["id"];
+    $query = "delete from receitas where id = " . $_GET["id"];
     $resultado = mysqli_query($conexao, $query);
     if ($resultado) {
-        header("Location: usuarios.php?sucesso=Excluído com sucesso");
+        header("Location: receitas.php?sucesso=Excluído com sucesso");
         exit();
     } else {
-        header("Location: usuarios.php?erro=Ocorreu algum erro");
+        header("Location: receitas.php?erro=Ocorreu algum erro");
         exit();
     }
 } else {
-    header("Location: usuarios.php?erro=Selecione um registro para excluir");
+    header("Location: receitas.php?erro=Selecione um registro para excluir");
     exit();
 }
 ?>
